5c314384bd70ad0f59fa1ba232f2e437d2acca23
ali-alim <ali.mamedov.2016@gmail.com>
2022-06-08 21:11:22 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Homepage,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/exchanges" element={<Exchanges />} />
+                <Route
+                  path="/cryptocurrencies"
+                  element={<Cryptocurrencies />}
+                />
+                <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                <Route path="/new" element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
+          <div className="footer">
+            <Typography.Title
+              level={5}
+              style={{ color: "blue", textAlign: "center" }}
+            >
+              Cryptoverse <br />
+              All rights reserved
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
